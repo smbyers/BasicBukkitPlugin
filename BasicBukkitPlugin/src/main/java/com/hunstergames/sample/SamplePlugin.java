100dd56ea5e0db97a378eed7ba46041c9a1d7691
Stephen Byers <smbyers@yahoo.com>
2015-10-03 23:16:02 -0500

    Added sample code for reading the plugin's config file

--- BasicBukkitPlugin/src/main/java/com/hunstergames/sample/SamplePlugin.java
+++ BasicBukkitPlugin/src/main/java/com/hunstergames/sample/SamplePlugin.java
@@ -1,5 +1,7 @@
 package com.hunstergames.sample;
 
+import java.util.List;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.hunstergames.sample.commands.EchoCommandExecutor;
@@ -11,8 +13,24 @@ public final class SamplePlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("onEnable has been invoked!");
-		new LoginListener(this); // create an event listenter which will register itself
-		this.getCommand(EchoCommandExecutor.CMD_NAME).setExecutor(new EchoCommandExecutor(this)); // register cmd executor
+		
+		// save a copy of the default config.xml if one is not there
+		this.saveDefaultConfig();
+		
+		// create an event listenter which will register itself
+		new LoginListener(this); 
+		
+		// register command executor
+		this.getCommand(EchoCommandExecutor.CMD_NAME).setExecutor(new EchoCommandExecutor(this));
+		
+		// read some configuration file values and print to log
+		// Reference: http://wiki.bukkit.org/Configuration_API_Reference
+		getLogger().info("Configuration value for 'greeting': " + this.getConfig().getString("greeting"));
+		List<String> days = this.getConfig().getStringList("days.ofweek");
+		getLogger().info("Days from config:");
+		for(String day : days) {
+			getLogger().info(day);
+		}
 	}
 	
 	@Override
